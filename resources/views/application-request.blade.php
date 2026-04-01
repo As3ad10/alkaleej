d28685deba4289e5c8e8fcf020c6e229046bbb80
As3ad Altabali <as3ad.moh@gmail.com>
2026-04-01 21:17:25 +0300

    chore: update Arabic translations and improve label visibility in application forms

--- resources/views/application-request.blade.php
+++ resources/views/application-request.blade.php
@@ -4,7 +4,7 @@
         class="relative flex min-h-screen items-center justify-center text-white before:absolute before:inset-0 before:z-0 before:bg-black/50"
         style="background-image: url('https://alkhaleej.com.sa/wp-content/uploads/2024/02/cover-12-min.png'); background-size: cover; background-position: bottom center; background-repeat: no-repeat; background-attachment: fixed;">
         <div class="wrapper relative z-10 mt-36 flex max-w-4xl flex-col justify-center gap-12">
-            <h2 class="text-center text-3xl font-bold text-zinc-900">
+            <h2 class="text-center text-3xl font-bold text-white">
                 اصدار مشهد قبول
             </h2>
             <div class="text-center">
